fa5a9441b5ecaa5ab1fed8295d4c2a15f9f47185
Chris Dongmin Chen <chrischen.mc2@gmail.com>
2023-06-27 16:17:03 -0700

    Ex 11.9 Simple end to end tests

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -3,5 +3,11 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('pokemon page can be navigated to Blastoise', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('blastoise').click()
+    cy.location('pathname').should('eq', '/pokemon/blastoise')
+    cy.contains('83')
   })
 })
